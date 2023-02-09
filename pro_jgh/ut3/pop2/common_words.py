365e6623caa102d90762705a8388a4e90bec1ddc
Javier-Garcia-Hernandez <javigh1903@movistar.es>
2023-02-09 18:34:36 +0000

    clonado de clase

--- pro_jgh/ut3/pop2/common_words.py
+++ pro_jgh/ut3/pop2/common_words.py
@@ -9,16 +9,28 @@ def run(input_path: Path) -> bool:
     output_path = "data/common_words/output.txt"
     # TU CÓDIGO AQUÍ
     with open(input_path, "r") as f:
-        data_rows = f.readlines()
+        data_rows = [line.lower().strip().split() for line in f]
     matches = []
     for data_row1 in data_rows:
-        words1 = set(data_row1.strip().lower().split())
+        words1 = set(data_row1)
         for data_row2 in data_rows:
-            words2 = set(data_row2.strip().lower().split())
-            match_count = len(words1 & words2)
-            matches.append(f"{match_count}\n")
+            words2 = set(data_row2)
+            matches.append(len(words1 & words2))
     with open(output_path, "w") as f:
-        f.writelines(matches)
+        for match_count in matches:
+            f.write(f"{match_count}\n")
+    # versión del examen
+    #     data_rows = f.readlines()
+    # matches = []
+    # for data_row1 in data_rows:
+    #     words1 = set(data_row1.strip().lower().split())
+    #     for data_row2 in data_rows:
+    #         words2 = set(data_row2.strip().lower().split())
+    #         match_count = len(words1 & words2)
+    # aquí no se genera una estructura de datos para luego dar formato de salida,
+    # se genera la salida directamente...mejor separar ambos procesos --> desacoplar
+    # with open(output_path, "w") as f:
+    #     f.writelines(matches)
     return filecmp.cmp(output_path, "data/common_words/.expected", shallow=False)
 
 
